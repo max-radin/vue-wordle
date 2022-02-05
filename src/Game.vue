b59d8215f5edb6b6a4166a509927b37e4d7de22a
Max Radin <radin.max@gmail.com>
2022-02-05 12:01:29 -0500

    Adding LOTR wordlist

--- src/Game.vue
+++ src/Game.vue
@@ -179,10 +179,10 @@ function genResultGrid() {
     </div>
   </Transition>
   <header>
-    <h1>VVORDLE</h1>
+    <h1>Gandarfle</h1>
     <a
       id="source-link"
-      href="https://github.com/yyx990803/vue-wordle"
+      href="https://github.com/max-radin/vue-wordle"
       target="_blank"
       >Source</a
     >
